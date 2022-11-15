9b52d0076fd2d5cc7ec4a042493fc6908926263d
LuisFernandes664 <82168814+LuisFernandes664@users.noreply.github.com>
2022-11-15 23:42:30 +0000

    Endpoint uptade, test accounts #10 not work

--- src/services/account.js
+++ src/services/account.js
@@ -1,6 +1,9 @@
+const accounts = require('../routes/accounts');
+
 module.exports = (app) => {
   const save = (account) => {
-    return app.db('accounts').insert(account, '*');
+    return app.db('accounts')
+      .insert(account, '*');
   };
 
   const findAll = () => {
@@ -8,8 +11,24 @@ module.exports = (app) => {
   };
 
   const find = (filter = {}) => {
-    return app.db('accounts').where(filter).first();
+    return app.db('accounts')
+      .where(filter)
+      .first();
   };
 
-  return { save, findAll, find };
+  const update = (id, account) => {
+    return app.db(accounts)
+      .where({ id })
+      .update(account, '*');
+  };
+
+  const remove = (id) => {
+    return app.db('accounts')
+      .where({ id })
+      .del();
+  };
+
+  return {
+    save, findAll, find, update, remove,
+  };
 };
--- test/routes/accounts.test.js
+++ test/routes/accounts.test.js
@@ -15,7 +15,6 @@ test('Test #7 - Inserir contas', () => {
   return request(app).post(MAIN_ROUTE)
     .send({ name: 'Account #1', user_id: user.id })
     .then((res) => {
-      console.log(res.body);
       expect(res.status).toBe(201);
       expect(res.body.name).toBe('Account #1');
     });
@@ -41,3 +40,24 @@ test('Test #9 - Listar conta por ID', () => {
       expect(res.body.user_id).toBe(user.id);
     });
 });
+
+test('Test #10 - Alterar uma conta', () => {
+  return app.db('accounts')
+    .insert({ name: 'Account - Update', user_id: user.id }, ['id'])
+    .then((acc) => request(app).put(`${MAIN_ROUTE}/${acc[0].id}`)
+      .send({ name: 'Account updated' }))
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.name).toBe('Account updated');
+    });
+});
+
+// test('Test #11 - Remover conta', () => {
+//   return app.db('accounts')
+//     .insert({ name: 'Account - Remove', user_id: user.id }, ['id'])
+//     .then((acc) => request(app).delete(`${MAIN_ROUTE}/${acc[0].id}`)
+//       .send({ name: 'Account updated' }))
+//     .then((res) => {
+//       expect(res.status).toBe(204);
+//     });
+// });
